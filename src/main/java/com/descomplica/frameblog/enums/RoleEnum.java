05f2503efa4a1b1628a0c42fd0e86b56965ca410
Rian H. S. Ezequias <rianhenrique0408@gmail.com>
2024-10-29 19:20:34 -0300

    começado config project aula Crianção de Endpoints

---
+++ src/main/java/com/descomplica/frameblog/enums/RoleEnum.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.descomplica.frameblog.enums;
+
+/**
+ *
+ * @author rianh
+ */
+public enum RoleEnum {
+    
+}
